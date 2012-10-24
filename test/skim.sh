f42e0f164ba459356f7e584f2f87848276bdbbab
radziej <>
2012-10-24 12:30:59 +0000

    Workaround for crab/globus '+' problem Updated global tags Added 2012D

--- test/skim.sh
+++ test/skim.sh
@@ -11,14 +11,14 @@ submit_mc()
     TAG=`echo $1 | sed -e s:/AOD::g -e s:^/::g -e s:/:_:g `
     DS=$1
     shift
-    ./prepare.sh -f -m -s $DS -t ${TAG} -g "${GLOBALTAG}::All" -c SkimmingCuts.cfg ${*}
+    ./prepare.sh -f -m -s $DS -t ${TAG} -g "${GLOBALTAG}::All" -c SkimmingCuts.cfg ${*} 
 }
 
 # two obligatory parameters: the dataset and the json file, and any optional parameters
 submit_data()
 {
     # Remove starting slash and ending, replace slashes by pluses
-    TAG=`echo $1 | sed -e s:/AOD::g -e s:^/::g -e s:/:+:g `
+    TAG=`echo $1 | sed -e s:/AOD::g -e s:^/::g -e s:/:_:g `
     DS=$1
     JSON=$2
     shift
@@ -49,16 +49,21 @@ GLOBALTAG="FT_53_V10_AN1"
 
 # 2012 C PromptReco analysis, CMSSW_5_3_?????
 # https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions#Global_tags_for_prompt_reco_data
-GLOBALTAG="GR_P_V40_AN1"
+GLOBALTAG="GR_P_V40_AN2"
 #submit_data /DoubleMu/Run2012C-PromptReco-v1/AOD $JSON
+GLOBALTAG="GR_P_V41_AN2"
 #submit_data /DoubleMu/Run2012C-PromptReco-v2/AOD $JSON
+
+# 2012 D PromptReco analysis, CMSSW_5_3_??
+GLOBALTAG="GR_P_V42_AN2"
+#submit_data /DoubleMu/Run2012D-PromptReco-v1/AOD $JSON
+
 
 ######################################################################
 # MC Summer 12 - July ReReco
 
 # CMSSW_5_3_2_patch4
 GLOBALTAG="START53_V7F"
-
-#submit_mc /DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM
+submit_mc /DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM
 
 #submit_mc /soft332_m0eq1000_m12eq500_tanbeq20_sgnMueq1_A0eq0_lp211eq0.01_hw4000_CMSSW523p3_noPU_AOD_20.08.2012/sonnen-soft332_m0eq1000_m12eq500_tanbeq20_sgnMueq1_A0eq0_lp211eq0.01_hw4000_CMSSW523p3_noPU_AOD_20.08.2012-1c2ba989717b25223297e99c10c41dbd/USER
